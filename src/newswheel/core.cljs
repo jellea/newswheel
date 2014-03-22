5de5877195c73ef3cf077da4dfddb2f32e5b6812
Jelle Akkerman <jelle@jelleakkerman.nl>
2014-03-22 15:47:04 +0100

    Set main components, adding growlnotify and sablono

--- src/newswheel/core.cljs
+++ src/newswheel/core.cljs
@@ -1,13 +1,23 @@
 (ns newswheel.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [sablono.core :as html :refer-macros [html]]
+            [newswheel.components.circleview :as circleview]
+            [newswheel.components.reader :as reader]
+            ))
 
 (enable-console-print!)
 
 (def app-state (atom {:text "Hello world!"}))
 
+(defn main [state]
+  (om/component
+    (html
+      [:div
+        (om/build circleview/main state {})
+        (om/build reader/main state {})])))
+
 (om/root
-  (fn [app owner]
-    (dom/h1 nil (:text app)))
+  main
   app-state
   {:target (. js/document (getElementById "app"))})
